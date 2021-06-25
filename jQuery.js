17c309c0cc04cdb8f3d6a6f41127aaedeb161116
MarkYegon7 <mark.yegon@student.moringaschool.com>
2021-06-25 20:54:58 +0300

    commit changes

---
+++ jQuery.js
@@ -0,0 +1,16 @@
+var a;
+function show_hide()
+{
+    if(a==1)
+        {
+            document.getElementById("image").style.display="inline";
+            return a=0;
+        }
+
+    else
+        {
+            document.getElementById("image").style.display="nome"
+            return a=1
+        }
+
+
